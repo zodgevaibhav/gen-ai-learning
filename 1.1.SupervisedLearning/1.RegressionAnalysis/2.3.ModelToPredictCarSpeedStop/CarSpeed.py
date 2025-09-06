9dbb8181acf53ee57f3f0e3e818f4df38120e3e2
Vaibhav Zodge <zodgevaibhav@gmail.com>
2025-09-06 15:56:34 +0530

    learn(NonLinearRegression): Add one more example for non linear

---
+++ 1.1.SupervisedLearning/1.RegressionAnalysis/2.3.ModelToPredictCarSpeedStop/CarSpeed.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as ply
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+df = pd.read_csv('data.csv')
+
+df.info()
+
+corr = df['Speed'].corr(df['BrakingDistance'])
+print(corr)
+
+x = df.drop(columns=['BrakingDistance'], axis=1)
+y=df['BrakingDistance']
+
+# print(x)
+
+poly = PolynomialFeatures(degree=2)
+x_square = poly.fit_transform(x) 
+# print(x_square)
+
+model = LinearRegression()
+model.fit(x_square, y)
+
+output = model.predict(poly.fit_transform([[120]]))
+
+print("Braking distance at speed 120 is : " )
+print(poly.fit_transform([[120]]))
+
+ply.plot(df['Speed'], df['BrakingDistance'])
+ply.xlabel('Speed')
+ply.ylabel('BrakingDistance')
+ply.title('Speed vs BrakingDistance')
+ply.show()
+
+# 617*617 = 380689
