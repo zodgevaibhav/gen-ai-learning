7cb8ff9d977198bf97447b79dd7c329d3a0bef9e
Vaibhav Zodge <zodgevaibhav@gmail.com>
2025-10-05 19:33:03 +0530

    lean(GenAI): Language translation model demo

--- 4.GenAI-ChatbotAndModelBuild/2.GenAIUsingHuggingFace_Transformer_Diffusers/5.LanguagteTranslation/LanguageTranslation.py
+++ 4.GenAI-ChatbotAndModelBuild/2.GenAIUsingHuggingFace_Transformer_Diffusers/5.LanguagteTranslation/LanguageTranslation.py
@@ -1,6 +1,10 @@
 from transformers import pipeline, logging
+import tkinter as tk
+from tkinter import messagebox
 
 logging.set_verbosity_error()
+
+# root = tk.Tk()
 
 # ---------------------------
 # Step 1: Define model for Marathi
@@ -14,14 +18,10 @@ translator = pipeline("translation_en_to_marathi", model=MODELS['marathi'], devi
 # ---------------------------
 # Step 2: Translate sentences
 # ---------------------------
-texts = [
-    "Hello, how are you?",
-    "I love programming in Python.",
-    "The weather today is very pleasant."
-]
+texts = "I love programming in Python."
 
-for text in texts:
-    translation = translator(text, max_length=100)
-    print("English:", text)
-    print("Marathi:", translation[0]['translation_text'])
-    print()
+translation = translator(texts, max_length=100)
+print("English:", texts)
+print("Marathi:", translation[0]['translation_text'])
+
+
